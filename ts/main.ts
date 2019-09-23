b0c22aa260f08f993e88824bdcaaf46818cc5ecb
1138-4EB <1138-4EB@users.noreply.github.com>
2019-09-23 23:57:59 +0200

    use 'file-type' to detect content-type mime

--- ts/main.ts
+++ ts/main.ts
@@ -2,8 +2,13 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 import { createReadStream, statSync } from 'fs';
-import { basename, extname, join, isAbsolute } from 'path';
+import { basename, join } from 'path';
 import { Glob } from 'glob';
+
+import * as readChunk from 'read-chunk';
+// FIXME Is it possible to import 'file-type'? e.g.:
+// import * as fileType from 'file-type';
+const fileType = require('file-type');
 
 export async function run() {
   try {
@@ -36,41 +41,27 @@ export async function run() {
     console.log(release);
     core.endGroup()
 
-    const options = {
-      'json': 'text/json',
-      'zip':  'application/zip',
-      'txt':  'text/plain',
-      'md':   'text/markdown',
-      'ts':   'application/typescript',
-      'gz':   'applicatiom/gzip'
-    }
-
     const xcwd = core.getInput('cwd', {required: true});
 
     core.getInput('files', {required: true}).split(/[\r\n]+/).forEach(async function(item){
-
       Glob(item, { cwd: xcwd }, async function (er, files) {
-
         if (er != null) {
           core.setFailed(er.message);
           throw er;
         }
-
         if (files.length == 0) {
           console.log('WARNING! Glob patter <' + item + '> produced an empty file list');
         }
-
         files.forEach(async function(name){
-
           const file = join(xcwd, name);
           const stats = statSync(file);
-
+          // FIXME Can we use some built-in feature instead of depending on 'read-chunk'?
+          const buffer = readChunk.sync(file, 0, fileType.minimumBytes);
           console.log('Upload ' + file + ' [size: ' + stats.size + ']...');
-
           await octokit.repos.uploadReleaseAsset({
             url: release.upload_url,
             headers: {
-              'content-type': options[extname(file).substr(1)],
+              'content-type': fileType(buffer).mime,
               'content-length': stats.size
             },
             name: basename(file),
