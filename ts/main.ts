49143f71430a6853f388ed92a8a2c10f8823f35b
1138-4EB <1138-4EB@users.noreply.github.com>
2019-09-23 22:57:26 +0200

    add warning when glob pattern produces empty file list

--- ts/main.ts
+++ ts/main.ts
@@ -56,6 +56,10 @@ export async function run() {
           throw er;
         }
 
+        if (files.length == 0) {
+          console.log('WARNING! Glob patter <' + item + '> produced an empty file list');
+        }
+
         files.forEach(async function(name){
 
           const file = join(xcwd, name);
